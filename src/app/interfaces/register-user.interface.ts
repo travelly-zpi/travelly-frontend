40e21d7ccdd4812992d91d3bce276bef875df9f5
Nazar Pechevystyi <nazarpechka@gmail.com>
2022-10-17 13:18:53 +0200

    Register and login v2 (#4)

--- src/app/interfaces/register-user.interface.ts
+++ src/app/interfaces/register-user.interface.ts
@@ -1,6 +1,6 @@
 export interface RegisterUserInterface {
-  name: string;
-  surname: string;
+  firstName: string;
+  lastName: string;
   email: string;
   password: string;
   passwordConfirm: string;
--- src/setupAxios.ts
+++ src/setupAxios.ts
@@ -1,3 +1,16 @@
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+axios.interceptors.request.use(
+  function (config) {
+    const user = JSON.parse(localStorage.getItem("user") || "null");
+    if (user) {
+      config.headers!["Authorization"] = "Bearer " + user.token;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
