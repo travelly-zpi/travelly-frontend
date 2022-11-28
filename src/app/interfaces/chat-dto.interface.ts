2fb15018cbd312058e5f182ab93111a390504c48
Alena Kirylchyk <48454522+kirilchikal@users.noreply.github.com>
2022-11-29 00:51:16 +0100

    Revert "Chat page" (#29)

--- src/app/interfaces/chat-dto.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface ChatDtoInterface {
-    uuid: string;
-    userUuid: string;
-    recipientUuid: string;
-    recipientName: string;
-    imageUrl: string;
-    newMessages: number;
-  }
